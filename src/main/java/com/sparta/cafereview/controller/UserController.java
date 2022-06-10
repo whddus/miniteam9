2e74b28e3dd51e4d609b66300a052adaeae85e9d
guluming <guluming@naver.com>
2022-06-10 18:56:28 +0900

    로그인 관련 API Controller 클래스 작성 / 로그아웃을 제외한 API 작성완료

--- src/main/java/com/sparta/cafereview/controller/UserController.java
+++ src/main/java/com/sparta/cafereview/controller/UserController.java
@@ -1,4 +1,48 @@
 package com.sparta.cafereview.controller;
 
+import com.sparta.cafereview.requestdto.UserRequestDto;
+import com.sparta.cafereview.responsedto.JwtResponseDto;
+import com.sparta.cafereview.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    //회원가입
+    @PostMapping("/signup")
+    public boolean signupUser(@RequestBody UserRequestDto singUpData){
+        return userService.signUpuser(singUpData);
+    }
+
+    //로그인
+    @PostMapping("/login")
+    public JwtResponseDto loginUser(@RequestBody UserRequestDto loginData){
+        return userService.loginUser(loginData);
+    }
+
+    //로그아웃
+    @GetMapping("/logout")
+    public String logout(){
+
+        return "로그아웃 되었습니다.";
+    }
+
+    //아이디 중복 검사
+    @PostMapping("/signup/useridCheck")
+    public boolean signupUseridCheck(@RequestBody UserRequestDto signupUseridCheckData){
+        return userService.signupUseridCheck(signupUseridCheckData);
+    }
+
+    //로그인 중복 확인
+    @GetMapping("/login/check")
+    public boolean loginUserCheck(){
+        Object authentication = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        return authentication.equals("anonymousUser");
+        return userService.loginUserCheck(authentication);
+    }
 }
