2958ae9b4c17566ee777d03052f4567f5ac3951b
guluming <guluming@naver.com>
2022-06-10 18:57:13 +0900

    사용자 회원가입 / 로그인 / ID중복 확인시 데이터 옮길 DTO 작성 / 작성완료

--- src/main/java/com/sparta/cafereview/requestdto/UserRequestDto.java
+++ src/main/java/com/sparta/cafereview/requestdto/UserRequestDto.java
@@ -1,4 +1,11 @@
 package com.sparta.cafereview.requestdto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class UserRequestDto {
+    private String userid;
+    private String password;
 }
