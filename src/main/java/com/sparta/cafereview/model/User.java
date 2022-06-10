e786807d349ddd2cd4f9c6a4bdc813ae754f215e
guluming <guluming@naver.com>
2022-06-10 18:54:51 +0900

    User 클래스 작성 / 암호화 함수 삽입, 사용자 권한 식별을 위한 Enum클래스 삽입

--- src/main/java/com/sparta/cafereview/model/User.java
+++ src/main/java/com/sparta/cafereview/model/User.java
@@ -1,4 +1,33 @@
 package com.sparta.cafereview.model;
 
+import com.sparta.cafereview.requestdto.UserRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
 public class User {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String userid;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User(UserRequestDto params){
+        userid = params.getUserid();
+        password = params.getPassword();
+        role = Role.USER;
+    }
+
+    public void encryptPassword(PasswordEncoder passwordEncoder) {
+        password = passwordEncoder.encode(password);
+    }
 }
