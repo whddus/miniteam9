8975673d11756bc9c00d50f0634b2eb5f495bc95
guluming <guluming@naver.com>
2022-06-13 00:19:05 +0900

    cafe 저장 Service 방식변경 및 저장확인 로직 재구성

--- src/main/java/com/sparta/cafereview/service/CafeService.java
+++ src/main/java/com/sparta/cafereview/service/CafeService.java
@@ -1,4 +1,5 @@
 package com.sparta.cafereview.service;
+
 import com.sparta.cafereview.model.Cafe;
 import com.sparta.cafereview.repository.CafeRepository;
 import com.sparta.cafereview.requestdto.CafeRequestDto;
@@ -9,8 +10,8 @@ import com.sparta.cafereview.responsedto.CafeResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.List;
 
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -20,45 +21,47 @@ public class CafeService {
 
     //저장
     @Transactional
-    public boolean saveCafe(CafeRequestDto requestDto,String userid) {
-        if (userid != null) {
-            Cafe cafe = new Cafe(requestDto,userid);
-        cafeRepository.save(cafe);
+    public boolean saveCafe(CafeRequestDto requestDto) {
+        //저장 유무를 판단 (영속성 컨텍스트 확인)
+        Cafe beforeSaveCafe = new Cafe(requestDto);
+        Cafe SaveCafe = cafeRepository.save(beforeSaveCafe);
         return true;
-        }
-        return false;
     }
+
     //전체조회
     public List<CafeResponseDto> getCafeList() {
         List<CafeResponseDto> cafeList = cafeRepository.findAllByOrderByIdDesc();
         return cafeList;
     }
+
     //수정
     @Transactional
-    public Boolean update(Long cafeid, CafeUpdateDto requestDto,String userid) {
+    public Boolean update(Long cafeid, CafeUpdateDto requestDto, String userid) {
         Cafe cafe = cafeRepository.findById(cafeid).orElseThrow(
                 () -> new IllegalArgumentException("카페가 존재하지 않습니다.")
         );
-        if(cafe.getUserid().equals(userid)){
+        if (cafe.getUserid().equals(userid)) {
             cafe.update(requestDto);
             return true;
         }
         return false;
     }
+
     //검색
     public List<CafeResponseDto> sortByCoffeebeanname(String coffeebeanname) {
-        List<CafeResponseDto> cafe = cafeRepository.findAllByCoffeebeannameOrderByIdDesc(coffeebeanname );
+        List<CafeResponseDto> cafe = cafeRepository.findAllByCoffeebeannameOrderByIdDesc(coffeebeanname);
         return cafe;
     }
+
     //삭제
-    public Boolean deleteCafe(Long cafeid,String userid) {
+    public Boolean deleteCafe(Long cafeid, String userid) {
         Cafe cafe = cafeRepository.findById(cafeid).orElseThrow(
                 () -> new IllegalArgumentException("카페가 존재하지 않습니다.")
         );
-        if(cafe.getUserid().equals(userid)){
-        cafeRepository.deleteById(cafeid);
+        if (cafe.getUserid().equals(userid)) {
+            cafeRepository.deleteById(cafeid);
 
-        return true;
+            return true;
         }
         return false;
     }
@@ -74,9 +77,9 @@ public class CafeService {
 
         List<CafeDetailReplyResponseDto> reply = replyRepository.findAllBycafeid(cafeid);
 
-        return new CafeDetailResponseDto(cafe.getCoffeebeanname(),cafe.getCafename(),cafe.getImageUrl(),
+        return new CafeDetailResponseDto(cafe.getCoffeebeanname(), cafe.getCafename(), cafe.getImageUrl(),
                 cafe.getCafereview(), reply
-                );
+        );
     }
 
 }
