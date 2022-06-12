c9cc893a92b3606ae3150056ce2ac4c8bb3661f3
guluming <guluming@naver.com>
2022-06-13 00:17:19 +0900

    cafe 테이블 저장방식 변경에 의한 DTO 수정

--- src/main/java/com/sparta/cafereview/requestdto/CafeRequestDto.java
+++ src/main/java/com/sparta/cafereview/requestdto/CafeRequestDto.java
@@ -1,12 +1,15 @@
 package com.sparta.cafereview.requestdto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 
+@Setter
 @Getter
 public class CafeRequestDto {
     private String cafename;
     private String coffeebeanname;
     private String imageUrl;
     private String cafereview;
+    private String userid;
 }
