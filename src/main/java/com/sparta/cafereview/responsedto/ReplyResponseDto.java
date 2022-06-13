705a856f98b9000faa4683d8cead6e1521674688
guluming <guluming@naver.com>
2022-06-13 23:46:41 +0900

    댓글 조회시 userid 추가

--- src/main/java/com/sparta/cafereview/responsedto/ReplyResponseDto.java
+++ src/main/java/com/sparta/cafereview/responsedto/ReplyResponseDto.java
@@ -12,12 +12,14 @@ public class ReplyResponseDto {
     private Long id;
 
     private String nickname;
+    private String userid;
 
     private String reply;
 
     public ReplyResponseDto(Reply reply){
         this.id = reply.getId();
         this.nickname = reply.getNickname();
+        this.userid = reply.getUserid();
         this.reply = reply.getReply();
     }
 }
