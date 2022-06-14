a9d3b668ab78031e780530984243062828028c9c
guluming <guluming@naver.com>
2022-06-14 22:54:16 +0900

    사용하지 않는 함수 삭제

--- src/main/java/com/sparta/cafereview/repository/ReplyRepository.java
+++ src/main/java/com/sparta/cafereview/repository/ReplyRepository.java
@@ -1,18 +1,11 @@
 package com.sparta.cafereview.repository;
 
 import com.sparta.cafereview.model.Reply;
-import com.sparta.cafereview.responsedto.CafeDetailReplyResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
-
+    Reply findByCafeidAndId(Long cafeid, Long id);
     List<Reply> findAllByCafeid(Long cafeid);
-//    Optional<Reply> findById(Long id);
-
-    List<CafeDetailReplyResponseDto> findAllBycafeid(Long cafeid);
-
- 
 }
